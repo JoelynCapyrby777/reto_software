908757f81fb46bd32225fb20a6bc88ed962df0a9
joelyn <yoel.canul.yacg@gmail.com>
2026-09-13 17:42:18 -0500

    feat: añadiendo conexion a la base de datos y ping route

--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config();
 
+// Importación de rutas
+const pingRoutes = require('./routes/pingRoutes');
+
+// Inicializando Express
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors()); 
-app.use(express.json()); 
-
-
+app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Conexi´m de rutas
+app.use('/', pingRoutes);
+
+// Arramnque del servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
