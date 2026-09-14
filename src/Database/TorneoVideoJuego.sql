a3657c695922da445741dd100fce51ef04a6f161
RenataCortes <cortesrenata1708@gmail.com>
2026-09-13 22:56:35 -0500

    refactor(base-datos): Actualizando tablas y llaves foráneas

--- src/Database/TorneoVideoJuego.sql
+++ src/Database/TorneoVideoJuego.sql
@@ -2,31 +2,34 @@ CREATE DATABASE TorneoVideoJuego;
 
 USE TorneoVideoJuego;
 
-CREATE TABLE Usuario(
+CREATE TABLE Usuario (
     idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-    Nombre VARCHAR(50),
+    Nombre VARCHAR(50) NOT NULL,
     Gamertag VARCHAR(50) NOT NULL UNIQUE,
-    Correo VARCHAR(50),
-    Contraseña VARCHAR(50),
+    Correo VARCHAR(50) NOT NULL,
     FechaRegistro DATETIME DEFAULT CURRENT_TIMESTAMP,
     /* 0 = false para jugador, 1 = true para administrador*/
     IsAdmin BOOLEAN NOT NULL DEFAULT 0
 );
 
-CREATE TABLE VideoJuego(
+CREATE TABLE VideoJuego (
     idVideoJuego INT PRIMARY KEY AUTO_INCREMENT,
-    fkUsuario INT,
     Nombre VARCHAR(100) NOT NULL UNIQUE,
-    Genero VARCHAR(50),
-    FOREIGN KEY (fkUsuario) REFERENCES Usuario(idUsuario)
+    Genero VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Puntuacion(
+CREATE TABLE Puntuacion (
     idPuntuacion INT PRIMARY KEY AUTO_INCREMENT,
-    fkUsuario INT,
-    fkVideoJuego INT,
-    Puntuacion DECIMAL (5,2),
+    fkUsuario INT NOT NULL,
+    fkVideoJuego INT NOT NULL,
+    Puntuacion INT UNSIGNED NOT NULL,
     FechaPuntuacion DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (fkUsuario) REFERENCES Usuario(idUsuario),
-    FOREIGN KEY (fkVideoJuego) REFERENCES VideoJuego(idVideoJuego)
+
+    CONSTRAINT fk_puntuacion_usuario
+        FOREIGN KEY (fkUsuario)
+        REFERENCES Usuario(idUsuario),
+
+    CONSTRAINT fk_puntuacion_videojuego
+        FOREIGN KEY (fkVideoJuego)
+        REFERENCES VideoJuego(idVideoJuego)
 );
